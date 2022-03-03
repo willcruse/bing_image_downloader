0d5a3b1f62df8ee3542cbcfc7ff22ff4763d02d0
Will Cruse <willcruse123@protonmail.com>
2022-03-03 12:09:19 +0000

    add yaml support

--- bing_image_downloader/bing.py
+++ bing_image_downloader/bing.py
@@ -7,15 +7,25 @@ import re
 
 import yaml
 
-'''
+"""
 Python api to download image form Bing.
 Author: Guru Prasad (g.gaurav541@gmail.com)
 Modified by: Will Cruse (wpjc20@bath.ac.uk)
-'''
+"""
 
 
 class Bing:
-    def __init__(self, query, limit, output_dir: Path, adult, timeout,  filter='', verbose=True, store_image_info=True):
+    def __init__(
+        self,
+        query,
+        limit,
+        output_dir: Path,
+        adult,
+        timeout,
+        filter="",
+        verbose=True,
+        store_image_info=True,
+    ):
         self.download_count = 0
         self.query = query
         self.output_dir = output_dir
@@ -24,7 +34,7 @@ class Bing:
         self.verbose = verbose
         self.store_image_info = store_image_info
         self.seen = set()
-        self.files = []
+        self.files = {}
 
         assert type(limit) == int, "limit must be integer"
         self.limit = limit
@@ -33,65 +43,82 @@ class Bing:
 
         # self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
         self.page_counter = 0
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
-      'AppleWebKit/537.11 (KHTML, like Gecko) '
-      'Chrome/23.0.1271.64 Safari/537.11',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-      'Accept-Encoding': 'none',
-      'Accept-Language': 'en-US,en;q=0.8',
-      'Connection': 'keep-alive'}
-
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
+            "AppleWebKit/537.11 (KHTML, like Gecko) "
+            "Chrome/23.0.1271.64 Safari/537.11",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3",
+            "Accept-Encoding": "none",
+            "Accept-Language": "en-US,en;q=0.8",
+            "Connection": "keep-alive",
+        }
 
     def get_filter(self, shorthand):
-            if shorthand == "line" or shorthand == "linedrawing":
-                return "+filterui:photo-linedrawing"
-            elif shorthand == "photo":
-                return "+filterui:photo-photo"
-            elif shorthand == "clipart":
-                return "+filterui:photo-clipart"
-            elif shorthand == "gif" or shorthand == "animatedgif":
-                return "+filterui:photo-animatedgif"
-            elif shorthand == "transparent":
-                return "+filterui:photo-transparent"
-            else:
-                return ""
-
+        if shorthand == "line" or shorthand == "linedrawing":
+            return "+filterui:photo-linedrawing"
+        elif shorthand == "photo":
+            return "+filterui:photo-photo"
+        elif shorthand == "clipart":
+            return "+filterui:photo-clipart"
+        elif shorthand == "gif" or shorthand == "animatedgif":
+            return "+filterui:photo-animatedgif"
+        elif shorthand == "transparent":
+            return "+filterui:photo-transparent"
+        else:
+            return ""
 
     def save_image(self, link, file_path):
         request = urllib.request.Request(link, None, self.headers)
         image = urllib.request.urlopen(request, timeout=self.timeout).read()
         if not imghdr.what(None, image):
-            print('[Error]Invalid image, not saving {}\n'.format(link))
-            raise ValueError('Invalid image, not saving {}\n'.format(link))
-        with open(str(file_path), 'wb') as f:
+            print("[Error]Invalid image, not saving {}\n".format(link))
+            raise ValueError("Invalid image, not saving {}\n".format(link))
+        with open(str(file_path), "wb") as f:
             f.write(image)
         if self.store_image_info:
-            self.files.append({
+            self.files[file_path.name] = {
                 "file-name": file_path.name,
                 "link": link,
                 "image-id": self.download_count,
                 "search-term": self.query,
-            })
-            print(self.files)
+            }
 
-    
     def download_image(self, link):
         self.download_count += 1
         # Get the image link
         try:
             path = urllib.parse.urlsplit(link).path
-            filename = posixpath.basename(path).split('?')[0]
+            filename = posixpath.basename(path).split("?")[0]
             file_type = filename.split(".")[-1]
-            if file_type.lower() not in ["jpe", "jpeg", "jfif", "exif", "tiff", "gif", "bmp", "png", "webp", "jpg"]:
+            if file_type.lower() not in [
+                "jpe",
+                "jpeg",
+                "jfif",
+                "exif",
+                "tiff",
+                "gif",
+                "bmp",
+                "png",
+                "webp",
+                "jpg",
+            ]:
                 file_type = "jpg"
-                
+
             if self.verbose:
                 # Download the image
-                print("[%] Downloading Image #{} from {}".format(self.download_count, link))
-                
-            self.save_image(link, self.output_dir.joinpath("Image_{}.{}".format(
-                str(self.download_count), file_type)))
+                print(
+                    "[%] Downloading Image #{} from {}".format(
+                        self.download_count, link
+                    )
+                )
+
+            self.save_image(
+                link,
+                self.output_dir.joinpath(
+                    "Image_{}.{}".format(str(self.download_count), file_type)
+                ),
+            )
             if self.verbose:
                 print("[%] File Downloaded !\n")
 
@@ -99,24 +126,36 @@ class Bing:
             self.download_count -= 1
             print("[!] Issue getting: {}\n[!] Error:: {}".format(link, e))
 
-    
     def run(self):
         while self.download_count < self.limit:
             if self.verbose:
-                print('\n\n[!!]Indexing page: {}\n'.format(self.page_counter + 1))
+                print("\n\n[!!]Indexing page: {}\n".format(self.page_counter + 1))
             # Parse the page source and download pics
-            request_url = 'https://www.bing.com/images/async?q=' + urllib.parse.quote_plus(self.query) \
-                          + '&first=' + str(self.page_counter) + '&count=' + str(self.limit) \
-                          + '&adlt=' + self.adult + '&qft=' + ('' if self.filter is None else self.get_filter(self.filter))
+            request_url = (
+                "https://www.bing.com/images/async?q="
+                + urllib.parse.quote_plus(self.query)
+                + "&first="
+                + str(self.page_counter)
+                + "&count="
+                + str(self.limit)
+                + "&adlt="
+                + self.adult
+                + "&qft="
+                + ("" if self.filter is None else self.get_filter(self.filter))
+            )
             request = urllib.request.Request(request_url, None, headers=self.headers)
             response = urllib.request.urlopen(request)
-            html = response.read().decode('utf8')
-            if html ==  "":
+            html = response.read().decode("utf8")
+            if html == "":
                 print("[%] No more images are available")
                 break
-            links = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+            links = re.findall("murl&quot;:&quot;(.*?)&quot;", html)
             if self.verbose:
-                print("[%] Indexed {} Images on Page {}.".format(len(links), self.page_counter + 1))
+                print(
+                    "[%] Indexed {} Images on Page {}.".format(
+                        len(links), self.page_counter + 1
+                    )
+                )
                 print("\n===============================================\n")
 
             for link in links:
